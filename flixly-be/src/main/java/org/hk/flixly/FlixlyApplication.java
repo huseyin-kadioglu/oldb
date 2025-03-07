e2fb7424a0574c268009f723430b48aec0a049c7
HK <huseyinavnikadioglu@gmail.com>
2025-03-07 23:50:45 +0300

    initial push

---
+++ flixly-be/src/main/java/org/hk/flixly/FlixlyApplication.java
@@ -0,0 +1,13 @@
+package org.hk.flixly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixlyApplication.class, args);
+	}
+
+}
---
+++ flixly-be/src/test/java/org/hk/flixly/FlixlyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hk.flixly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlixlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
