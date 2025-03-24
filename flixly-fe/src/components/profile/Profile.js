b2b749fafc6cc30456488cf2092aa3fa7e1e60bf
HK <huseyinavnikadioglu@gmail.com>
2025-03-24 03:03:47 +0300

    improving profile stats css

--- flixly-fe/src/components/profile/Profile.js
+++ flixly-fe/src/components/profile/Profile.js
@@ -11,30 +11,24 @@ import Image from "../frame/Image";
 
 const Profile = () => {
   return (
-    <div className="container">
+    <div>
       <div className="profile">
         <div className="profile-summary">
           <div className="user-info">
             <Image />
-
-            <div className="user-info">
-              <p className="username">huseyinkadioglu</p>
-              <p className="location">İstanbul, Türkiye</p>
-              <a href="#" className="profile-link">
-                Profili Gör
-              </a>
-            </div>
+            <p className="username">huseyinkadioglu</p>
+            <p className="location">İstanbul, Türkiye</p>
           </div>
 
           <div className="stats">
             <div className="stat">
-              <p className="stat-number">100</p>
-              <p className="stat-label">Toplam</p>
+              <span className="value">100</span>
+              <span className="definition">TOTAL</span>
             </div>
             <div className="stat-divider"></div> {/* Uzun çubuk */}
             <div className="stat">
-              <p className="stat-number">10</p>
-              <p className="stat-label">Bu Yıl</p>
+              <span className="value">10</span>
+              <span className="definition">THIS YEAR</span>
             </div>
           </div>
         </div>
